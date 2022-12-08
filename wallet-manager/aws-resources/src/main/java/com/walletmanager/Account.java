625e832d37a6e810005b8da8544cbc3ae0a32268
Andre Lucas <andrelucastic@gmail.com>
2022-12-08 23:43:26 +0000

    project configuration

---
+++ wallet-manager/aws-resources/src/main/java/com/walletmanager/Account.java
@@ -0,0 +1,26 @@
+package com.walletmanager;
+
+import software.constructs.Construct;
+
+import java.util.Objects;
+
+public class Account {
+    private final Construct construct;
+    public Account(Construct construct) {
+        this.construct = construct;
+    }
+
+    public String getRegion() {
+        String region = (String) construct.getNode().tryGetContext("region");
+        Objects.requireNonNull(region, "region must be not null");
+
+        return region;
+    }
+
+    public String getAccountId() {
+        String accountId = (String) construct.getNode().tryGetContext("accountId");
+        Objects.requireNonNull(accountId, "accountId must be not null");
+
+        return accountId;
+    }
+}
---
+++ wallet-manager/aws-resources/src/main/java/com/walletmanager/AwsApp.java
@@ -0,0 +1,38 @@
+package com.walletmanager;
+
+import software.amazon.awscdk.App;
+import software.amazon.awscdk.Environment;
+import software.amazon.awscdk.StackProps;
+
+public class AwsApp {
+    public static void main(final String[] args) {
+        App app = new App();
+        Account account = new Account(app);
+
+        String accountId = account.getAccountId();
+        String region = account.getRegion();
+
+
+        executeStacks(app, accountId, region);
+
+        app.synth();
+    }
+    private static void executeStacks(App app, String accountId, String region) {
+
+        new WalletManagerStack(app, "wallet-manager-stack",
+                    StackProps.builder().env(getEnv(accountId, region))
+                            .build());
+
+        app.synth();
+    }
+
+    public static Environment getEnv(final String accountId,
+                                     final String region){
+
+        return Environment.builder()
+                .account(accountId)
+                .region(region)
+                .build();
+    }
+}
+
