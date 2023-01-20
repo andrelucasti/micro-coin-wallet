94f740f866e25c6e7818789510c0159a4f783f1c
Andre Lucas <andrelucastic@gmail.com>
2023-01-20 18:46:56 +0000

    add ecs tack

--- wallet-transaction/wallet-transaction-aws-resources/src/main/java/com/crypto/WalletTransactionAppStack.java
+++ wallet-transaction/wallet-transaction-aws-resources/src/main/java/com/crypto/WalletTransactionAppStack.java
@@ -1,5 +1,6 @@
 package com.crypto;
 
+import com.crypto.wallettransaction.ecs.EcsStack;
 import com.crypto.wallettransaction.sqs.SqsStack;
 import com.crypto.wallettransaction.sqs.queues_subscribed.QueuesSubscribedStack;
 import org.jetbrains.annotations.Nullable;
@@ -8,6 +9,7 @@ import software.amazon.awscdk.StackProps;
 import software.constructs.Construct;
 
 public class WalletTransactionAppStack extends Stack {
+    private static final String VPC_NAME = "aws-resources-lab";
     public WalletTransactionAppStack(final Construct scope,
                                      final String id,
                                      final StackProps props) {
@@ -19,5 +21,9 @@ public class WalletTransactionAppStack extends Stack {
         );
 
         sqsStack.execute();
+
+        var environment = Environment.SANDBOX;
+        var vpcName = environment.withResourceName(VPC_NAME);
+        new EcsStack(this, "wallet-transaction-ecs-stack", props, Environment.SANDBOX, vpcName);
     }
 }
